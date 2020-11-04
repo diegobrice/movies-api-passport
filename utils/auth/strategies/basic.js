5de0694390ab3e924961bc2915d7e0717e781f6b
Diego Briceño <diegobrice@hotmail.com>
2020-11-03 19:38:07 -0500

    sign-in added

--- utils/auth/strategies/basic.js
+++ utils/auth/strategies/basic.js
@@ -9,7 +9,7 @@ passport.use(
   new BasicStrategy(async function(email, password, cb) {
     const userService = new UsersService();
     try {
-      const user = userService.getUser({ email });
+      const user = await userService.getUser({ email });
       if (!user) {
         return cb(boom.unauthorized(), false);
       }
